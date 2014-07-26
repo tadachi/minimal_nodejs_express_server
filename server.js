da0f735212fe070c5eec4332dd8ec96f6a85bde5
Takumi Adachi <takumiadachi@gmail.com>
2014-07-26 10:38:36 -0700

    Added output to console IP with a timestamp.  Added comments.

--- server.js
+++ server.js
@@ -18,6 +18,15 @@ var vhost           = require('vhost');
 var app             = require('express.io')();
 var port            = parseInt(process.env.PORT, 10) || 4000;
 
+// Simple timestamp function. Invoke with timestamp();
+htimestamp = function() {
+    var date = new Date();
+    result = '[' + date.getFullYear() + '/' + date.getMonth() + '/' +
+        date.getDate() + '/' + date.getHours() + ':' +
+        date.getMinutes() + ':' + date.getSeconds() + ']';
+    return result;
+}
+
 app.http().io(); // Initialize the server.
 
 /*
@@ -30,8 +39,12 @@ app.use(bodyParser.urlencoded());
 // Parse application/json
 app.use(bodyParser.json());
 
+// Do not tell header that server is powered by Node.js
+app.disable('x-powered-by');
+
 // Simulate HTTP DELETE and PUT.
-//app.use(methodOverride());
+// Only uncomment is you want to use PUT and DELETE.
+// app.use(methodOverride());
 
 app.listen(port); // Listen through the specified port.
 //app.enable('trust proxy'); // Enables reverse proxy support Default: false
@@ -46,22 +59,9 @@ var home = require('express.io')();
 // This has to be relative to where your html files are located, etc. In this case it is in App.
 home.use('/js', express.static(__dirname + '/app/js'));
 home.use('/css', express.static(__dirname + '/app/css'));;
-//home.use('/img', express.static(__dirname + '/app/img'));
+home.use('/img', express.static(__dirname + '/app/img'));
 
 home.set('jsonp callback', true);
-
-/* Add specific headers before you send a response to your client. */
-//home.use(function (req, res, next) {
-    // Website you wish to allow to connect via CORS
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-
-    // Request methods you wish to allow
-//	res.setHeader('Access-Control-Allow-Methods', 'GET');
-
-    // Pass to next layer of middleware
-    //next();
-//});
 
 var hostname = 'localhost'; // Set the hostname for your website.
 app.use(vhost(hostname, home)); // Vhost allows you to host multiple websites on the same server.
@@ -75,7 +75,7 @@ home.get('/', function(req, res) {
 
 /* Outputs the users' ips visiting your website. */
 app.io.route('home', function (req) {
-    console.log(req.ip);
+    console.log(htimestamp() + ' ' + req.ip);
 });
 
 /* Debug */
